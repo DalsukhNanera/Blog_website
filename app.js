90dc59d7962f5f1e2ce8df484b737b5bf298bff1
Dalsukh <naneradalsukh@gmail.com>
2023-10-30 19:28:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,116 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB").then(()=>{console.log("connected")}).catch(e=>console.log("nathi thyu"));
+
+
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
+app.get("/", function(req, res){
+
+
+
+  Post.find({}).then( (posts)=>
+  {
+//   if (typeof posts === 'undefined') {
+//         posts = []; 
+//       }
+     res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    })
+  }).catch(err=>console.log("there is error") );
+ 
+ });
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+ 
+
+  post.save()
+  .then(() => res.redirect("/"))
+  .catch((err)=>{console.log("ok this is error")});
+
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+//   Post.findOne({_id: requestedPostId}, function(err, post){
+//     res.render("post", {
+//       title: post.title.tostring,
+//       content: post.content.tostring,
+//     });
+//   });
+
+Post.findOne({_id:requestedPostId}).then((post)=>{
+    res.render("post", {
+               post
+             });
+    })
+})
+
+
+app.post("/edit/:id",(req,res)=>{
+    Post.findOne({_id:req.params.id}).then((post)=>{
+        res.render("edit",{post});
+    }).catch(e=>{res.send("something happen")});
+})
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.put("/edit/:id",(req,res)=>{
+   Post.updateOne({_id:req.params.id},{content:req.body.editedpost}).then(()=>{
+    res.redirect("/");
+   }).catch(e=>res.send("something not working"));
+})
+
+app.delete("/delete/:id",(req,res)=>{
+    Post.deleteOne({_id:req.params.id}).then(()=>{
+     res.redirect("/");
+    }).catch(e=>res.send("something not working"));
+ })
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
